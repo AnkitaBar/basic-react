576604d6500fe6c8a92b0ae6e69465a45d04c892
Ankita <ankita.bar1026@gmail.com>
2024-05-05 21:44:34 +0530

    todo list

---
+++ react-ludo/src/TodoList.jsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+
+export default function TodoList () {
+    let [todos, setTodos] = useState([{ task:"sample Task", id:uuidv4(), isDone: false}]);
+    let [newTodo,setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        //console.log("we have to add new task in todo");
+        setTodos(( prevTodos ) => {
+            return[...prevTodos,{task:newTodo, id: uuidv4(),isDone:false}];
+        });
+        setNewTodo("");
+    };
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value);
+    }
+
+    let deleteTodo = (id) => {
+       // console.log("task to be deleted");
+        setTodos((prevTodos)=>todos.filter((prevTodos) => prevTodos.id != id ));
+    };
+
+    // let UpperCaseAll = () =>{
+    //     setTodos((prevTodos)=>prevTodos.map((todo) =>{
+    //         return {
+    //             ...todo,
+    //             task: todo.task.toUpperCase(),
+
+    //         };
+    //     })
+    // );
+    // };
+
+    // let UpperCaseOne = (id) => {
+    //     setTodos((prevTodos)=>prevTodos.map((todo) =>{
+    //         if (todo.id == id){
+    //         return {
+    //             ...todo,
+    //             task: todo.task.toUpperCase(),
+
+    //         };
+    //     }else{
+    //         return(todo);
+    //     }
+    //     })
+    // );
+    // }
+    let markAllDone = () =>{
+        setTodos((prevTodos)=>prevTodos.map((todo) =>{
+            return {
+                ...todo,
+                isDone: true,
+
+            };
+        })
+    );
+    };
+
+    let markAsDone = (id) => {
+        setTodos((prevTodos)=>prevTodos.map((todo) =>{
+            if (todo.id == id){
+            return {
+                ...todo,
+                isDone: true,
+
+            };
+        }else{
+            return(todo);
+        }
+        })
+    );
+    }
+
+    return (
+        <div>
+            <input placeholder="add a task" value={newTodo} onChange={updateTodoValue}></input> <br /><br />
+            <button onClick={addNewTask}>Add Task</button>
+            <br />
+            <br />
+        
+        <hr />
+            <h4>Tasks Todo </h4>
+            <ul>
+                { todos.map((todo) => (
+                    <li key={todo.id}>
+                    <span style={todo.isDone ? { textDecorationLine: "line-through"} : {} }>{todo.task}</span>
+                    &nbsp;&nbsp;
+                    <button onClick = {() =>deleteTodo(todo.id)}>delete</button>
+
+                    {/* <button onClick = {() =>UpperCaseOne(todo.id)}>UpperCase One</button> */}
+
+                    <button onClick = {() =>markAsDone(todo.id)}>Mark as Done</button>
+                    </li>
+                    
+                ))}
+                
+            </ul>
+            {/* <button onClick={UpperCaseAll}>UpperCase All</button> */}
+
+            <button onClick={markAllDone}>Mark All Done</button>
+        </div>
+    );
+}
